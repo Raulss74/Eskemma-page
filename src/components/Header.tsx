ef1fa6992b40cf4ba05e319936f081515576af7b
Raulss74 <68406050+Raulss74@users.noreply.github.com>
2025-02-26 14:22:25 -0600

    Actz 25-02-26 Utiities Hover & Animacion Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 // src/components/Header.tsx
+
 import { useState } from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid"; // Importar el ícono de hamburguesa
 
@@ -22,80 +23,82 @@ const Header = () => {
         <div className="flex items-center space-x-4 relative">
           {/* Ícono de Hamburguesa */}
           <button
-            className="p-4 text-black-eske hover:text-orange-400 focus:outline-none"
+            className="cursor-pointer p-4 text-black-eske hover:text-blue-70 focus:outline-none"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
-            <Bars3Icon className="h-10 w-10 text-black-eske " /> {/* Ícono de hamburguesa */}
+            <Bars3Icon className="h-10 w-10 text-black-eske hover:text-blue-70" /> {/* Ícono de hamburguesa */}
           </button>
 
           {/* Texto y Avatar */}
           <div className="hidden md:flex items-center space-x-4">
-            <span className="text-16px font-medium hover:text-orange-400 cursor-pointer">
-              Regístrate
+            <span className="text-16px font-medium hover:text-blue-70 cursor-pointer">
+              REGÍSTRATE
             </span>
-            <span className="text-16px font-medium hover:text-orange-400 cursor-pointer">
-              Iniciar Sesión
+            <span className="text-16px font-medium hover:text-blue-70 cursor-pointer">
+              INICIAR SESIÓN
             </span>
-            <div className="w-14 h-14 bg-gray-300 rounded-full flex items-center justify-center">
+            <div className="w-14 h-14 bg-orange-400 rounded-full flex items-center justify-center">
               {/* Avatar del Usuario */}
               <span className="text-16px font-bold text-gray-600">JD</span> {/* Ejemplo de iniciales */}
             </div>
           </div>
 
           {/* Menú Desplegable (Hamburguesa) */}
-          {isMenuOpen && (
-            <div className="absolute top-full left-0 right-0 bg-white-eske shadow-md mt-2 py-4 z-10 text-2xl">
-              <nav>
-                <ul className="flex flex-col space-y-2 px-8">
-                  <li>
-                    <a href="/" className="hover:text-orange-400">
-                      Inicio
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/sefix" className="hover:text-orange-400">
-                      Sefix
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/canvas" className="hover:text-orange-400">
-                      Canvas
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/monitor" className="hover:text-orange-400">
-                      Monitor
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/cursos" className="hover:text-orange-400">
-                      Cursos
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/consultoria" className="hover:text-orange-400">
-                      Consultoría
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/elbauldefouche" className="hover:text-orange-400">
-                      El Baúl de Fouché
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/recursos" className="hover:text-orange-400">
-                      Recursos
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/contacto" className="hover:text-orange-400">
-                      Contacto
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          )}
+          <div
+            className={`absolute top-full left-0 right-0 bg-white-eske shadow-md mt-2 py-4 z-10 text-2xl transition-all duration-300 ${
+              isMenuOpen ? "opacity-100 visible" : "opacity-0 invisible"
+            }`}
+          >
+            <nav>
+              <ul className="flex flex-col space-y-2 px-8">
+                <li>
+                  <a href="/" className="hover:text-bluegreen-eske hover:font-bold">
+                    INICIO
+                  </a>
+                </li>
+                <li>
+                  <a href="/sefix" className="hover:text-bluegreen-eske hover:font-bold">
+                    SEFIX
+                  </a>
+                </li>
+                <li>
+                  <a href="/canvas" className="hover:text-bluegreen-eske hover:font-bold">
+                    CANVAS
+                  </a>
+                </li>
+                <li>
+                  <a href="/monitor" className="hover:text-bluegreen-eske hover:font-bold">
+                    MONITOR
+                  </a>
+                </li>
+                <li>
+                  <a href="/cursos" className="hover:text-bluegreen-eske hover:font-bold">
+                    CURSOS
+                  </a>
+                </li>
+                <li>
+                  <a href="/consultoria" className="hover:text-bluegreen-eske hover:font-bold">
+                    CONSULTORÍA
+                  </a>
+                </li>
+                <li>
+                  <a href="/elbauldefouche" className="hover:text-bluegreen-eske hover:font-bold">
+                    EL BAÚL DE FOUCHÉ
+                  </a>
+                </li>
+                <li>
+                  <a href="/recursos" className="hover:text-bluegreen-eske hover:font-bold">
+                    RECURSOS
+                  </a>
+                </li>
+                <li>
+                  <a href="/contacto" className="hover:text-bluegreen-eske hover:font-bold">
+                    CONTACTO
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
